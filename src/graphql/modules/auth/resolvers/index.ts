3c8cb23e90a24dbe85e655a8dc198ae7c51541d6
baijanathTharu <bnchaudhary.tharu@gmail.com>
2022-03-14 20:47:32 +0545

    handle signup error

--- src/graphql/modules/auth/resolvers/index.ts
+++ src/graphql/modules/auth/resolvers/index.ts
@@ -1,18 +1,29 @@
+import { Prisma } from '@prisma/client';
 import { AuthModule } from '../generated-types/module-types';
 import { createUser, loginUser } from '../services';
 
 export const authResolvers: AuthModule.Resolvers = {
   Mutation: {
     signUp: async (_, { signUpInput }) => {
-      const created = await createUser(signUpInput);
+      try {
+        const created = await createUser(signUpInput);
 
-      if (!created) {
-        throw new Error('Something went wrong while saving data');
+        if (!created) {
+          throw new Error('Something went wrong while saving data');
+        }
+
+        return {
+          done: true,
+        };
+      } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+          if (e.code === 'P2002') {
+            throw new Error('User already exists');
+          }
+        }
+
+        throw new Error(e as string);
       }
-
-      return {
-        done: true,
-      };
     },
     login: async (_, { loginInput }) => {
       await loginUser(loginInput);
