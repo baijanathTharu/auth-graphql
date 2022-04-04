44d2304334b068158b409c2c2c3c9f01c2f1d9cb
baijanathTharu <bnchaudhary.tharu@gmail.com>
2022-04-04 19:01:48 +0545

    chore: refactor abilities

---
+++ src/abilities/doctorAbility.ts
@@ -0,0 +1,26 @@
+import { User, Prescription, UserRole, Role } from '@prisma/client';
+import { AbilityClass, AbilityBuilder } from '@casl/ability';
+import { PrismaAbility, Subjects } from '@casl/prisma';
+
+type AppAbility = PrismaAbility<
+  [
+    string,
+    // @ts-ignore
+    Subjects<{
+      User: User;
+      UserRole: UserRole;
+      Prescription: Prescription;
+    }>
+  ]
+>;
+
+export const doctorAbility = (userRoles: Role[]) => {
+  const Ability = PrismaAbility as AbilityClass<AppAbility>;
+  const { can, build } = new AbilityBuilder(Ability);
+
+  if (userRoles.includes(Role.DOCTOR)) {
+    can('write', 'Prescription');
+  }
+
+  return build();
+};
